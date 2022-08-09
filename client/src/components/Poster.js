f15f7f90c93ce76345278777bbc9cddc74c5a764
Jerry Guerrero <JerryGDev126@gmail.com>
2022-08-09 15:14:12 -0700

    added switching pages and additional files

--- client/src/components/Poster.js
+++ client/src/components/Poster.js
@@ -3,7 +3,7 @@ import starWars from "../assets/images/sw.jpg";
 export default function Poster() {
   return (
     <div>
-      <img className="maxWidth100" src={starWars} alt="Star Wars image" />
+      <img className="maxWidth100" src={starWars} alt="Star Wars Poster" />
     </div>
   );
 }
---
+++ client/src/components/Poster2.js
@@ -0,0 +1,9 @@
+import madMax from "../assets/images/MadMaxPoster.jpg";
+
+export default function Poster() {
+  return (
+    <div>
+      <img className="maxWidth100" src={madMax} alt="Mad Max Poster" />
+    </div>
+  );
+}
