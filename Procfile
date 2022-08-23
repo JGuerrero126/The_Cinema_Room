be9f32621015a6fbe82a17cc7c712c8ad3f274d2
stuart-rickard <stuart@bau-dev.com>
2022-08-22 23:34:50 -0700

    added pymongo test routes

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn -w 4 "base.py" -t 120
--- server/base.py
+++ server/base.py
@@ -1,11 +1,34 @@
+from pymongo import MongoClient
 from flask import Flask
+from bson.json_util import dumps
 
-api = Flask(__name__)
+app = Flask(__name__)
 
-@api.route('/profile')
+client = MongoClient("mongodb://localhost:27017")
+db = client.flask_example_db
+
+@app.route("/create")
+def add_user():
+  result = db.users.insert_one({"name": "test name 01"})
+  return str(result.inserted_id)
+
+@app.route("/list")
+def get_user():
+  users = list(db.users.find({}))
+  return dumps(users)
+
+
+# from flask import Flask
+
+# api = Flask(__name__)
+
+@app.route('/profile')
 def my_profile():
+    users = list(db.users.find({}))
+    print(users[1]["name"])
+
     response_body = {
-        "name": "test 2",
+        "name": users[1]["name"],
         "about" :"Hello!"
     }
 
