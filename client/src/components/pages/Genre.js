a0e0ee44aab7abbb75e12e55a0dd242955b2860d
Jerry Guerrero <JerryGDev126@gmail.com>
2022-08-26 15:17:10 -0700

    added axios to every page, tuned them to get the requests they should

--- client/src/components/pages/Genre.js
+++ client/src/components/pages/Genre.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, Heading, Link, SimpleGrid, Box, Image } from "@chakra-ui/react";
+import axios from "axios";
+import { useParams } from "react-router";
 
 function Genre() {
+  const [profileData, setProfileData] = useState(null);
+  const [genreData, setGenreData] = useState(null);
+  const genre = useParams();
+
+  function getData() {
+    axios({
+      method: "GET",
+      url: "/profile",
+    })
+      .then((response) => {
+        const res = response.data;
+        setProfileData({
+          profile_name: res.name,
+          about_me: res.about,
+        });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+
+  function getGenre() {
+    axios({
+      method: "GET",
+      url: "/genres/" + genre,
+    })
+      .then((response) => {
+        const res = response.data;
+        console.log(res);
+        setGenreData(res);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+
   return (
     <div>
       <Heading fontSize="2rem">This is the Genre Page.</Heading>
@@ -11,6 +57,14 @@ function Genre() {
           <Link fontSize="1.5rem" href="/movies/test">
             Click here to go to the Movie Page.
           </Link>
+          <p>Test call to db for "profile": </p>
+          <button onClick={getData}>Click me</button>
+          {profileData && (
+            <div>
+              <p>Profile name: {profileData.profile_name}</p>
+              <p>About me: {profileData.about_me}</p>
+            </div>
+          )}
         </Box>
         <Box
           w="85%"
