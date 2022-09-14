8a20383a8f8929b30ce03e3363655370c79cf175
Jerry Guerrero <JerryGDev126@gmail.com>
2022-09-13 20:14:08 -0700

    enforced automation and style changes throughout

--- client/src/components/pages/Movie.js
+++ client/src/components/pages/Movie.js
@@ -27,7 +27,7 @@ function Movie() {
     })
       .then((response) => {
         const res = response.data;
-        setMovieData2({
+        setMovieData({
           title: res.title,
           genres: res.genres,
           rating: res.rating,
@@ -69,58 +69,125 @@ function Movie() {
   // }, []);
 
   useEffect(() => {
-    setMovieData([
-      {
-        Name: "John Cleese",
-        Character: "Alex Murphy/RoboCop",
-        Role: "ACTOR",
-        Image:
-          "https://m.media-amazon.com/images/M/MV5BMTE5MTIxNTM1NV5BMl5BanBnXkFtZTYwOTMzNjc1._V1_FMjpg_UX277_.jpg",
-        Person_id: "1549",
-      },
-      {
-        Name: "Nancy Allen",
-        Character: "Anne Lewis",
-        Role: "ACTOR",
-        Image:
-          "https://m.media-amazon.com/images/M/MV5BNzU3NTFjZjUtNzE1OS00YTA4LWI3ZmYtNTBiZGY3YmY0YjU5XkEyXkFqcGdeQXVyMjI3NDc1NTU@._V1_FMjpg_UX427_.jpg",
-        Person_id: "1234567",
-      },
-      {
-        Name: "Dan O'Herlihy",
-        Character: "The Old Man",
-        Role: "ACTOR",
-        Image:
-          "https://m.media-amazon.com/images/M/MV5BMjMwNjYxOTEzMl5BMl5BanBnXkFtZTcwOTA3NTUwOA@@._V1_.jpg",
-        Person_id: "1234568",
-      },
-      {
-        Name: "Ronny Cox",
-        Character: "Dick Jones",
-        Role: "ACTOR",
-        Image:
-          "https://m.media-amazon.com/images/M/MV5BMjEyMjAzNTI0M15BMl5BanBnXkFtZTcwNTA1MjcyMQ@@._V1_FMjpg_UX149_.jpg",
-        Person_id: "1234569",
-      },
-      {
-        Name: "Paul Verhoeven",
-        Character: "",
-        Role: "DIRECTOR",
-        Image:
-          "https://m.media-amazon.com/images/M/MV5BMTU5NTc4OTU0Nl5BMl5BanBnXkFtZTYwMDU2MDc0._V1_FMjpg_UX275_.jpg",
-        Person_id: "12345610",
-      },
-    ]);
+    getData();
   }, []);
+
+  // useEffect(() => {
+  //   setMovieData([
+  //     {
+  //       Name: "John Cleese",
+  //       Character: "Alex Murphy/RoboCop",
+  //       Role: "ACTOR",
+  //       Image:
+  //         "https://m.media-amazon.com/images/M/MV5BMTE5MTIxNTM1NV5BMl5BanBnXkFtZTYwOTMzNjc1._V1_FMjpg_UX277_.jpg",
+  //       Person_id: "1549",
+  //     },
+  //     {
+  //       Name: "Nancy Allen",
+  //       Character: "Anne Lewis",
+  //       Role: "ACTOR",
+  //       Image:
+  //         "https://m.media-amazon.com/images/M/MV5BNzU3NTFjZjUtNzE1OS00YTA4LWI3ZmYtNTBiZGY3YmY0YjU5XkEyXkFqcGdeQXVyMjI3NDc1NTU@._V1_FMjpg_UX427_.jpg",
+  //       Person_id: "1234567",
+  //     },
+  //     {
+  //       Name: "Dan O'Herlihy",
+  //       Character: "The Old Man",
+  //       Role: "ACTOR",
+  //       Image:
+  //         "https://m.media-amazon.com/images/M/MV5BMjMwNjYxOTEzMl5BMl5BanBnXkFtZTcwOTA3NTUwOA@@._V1_.jpg",
+  //       Person_id: "1234568",
+  //     },
+  //     {
+  //       Name: "Ronny Cox",
+  //       Character: "Dick Jones",
+  //       Role: "ACTOR",
+  //       Image:
+  //         "https://m.media-amazon.com/images/M/MV5BMjEyMjAzNTI0M15BMl5BanBnXkFtZTcwNTA1MjcyMQ@@._V1_FMjpg_UX149_.jpg",
+  //       Person_id: "1234569",
+  //     },
+  //     {
+  //       Name: "Paul Verhoeven",
+  //       Character: "",
+  //       Role: "DIRECTOR",
+  //       Image:
+  //         "https://m.media-amazon.com/images/M/MV5BMTU5NTc4OTU0Nl5BMl5BanBnXkFtZTYwMDU2MDc0._V1_FMjpg_UX275_.jpg",
+  //       Person_id: "12345610",
+  //     },
+  //   ]);
+  // }, []);
 
   // console.log(movieTest);
 
   return (
     <div>
-      <Heading fontSize="2rem">This is the Movie Page.</Heading>
+      {movieData && (
+        <div>
+          <Heading fontSize="2rem">{movieData.title}</Heading>
+        </div>
+      )}
       <SimpleGrid columns={2} width="100%" ml="2rem" mr="2rem">
         <Box>
-          <Text fontSize="2rem">Info on the Actors go Here.</Text>
+          {movieData && (
+            <div>
+              {/* <p>Title: {movieData.title}</p> */}
+              <p>Genres: {movieData.genres}</p>
+              <p>Rating: {movieData.rating}</p>
+              <p>Description: {movieData.description}</p>
+              <p>Release Year: {movieData.release_year}</p>
+              {/* <p>Actor Array: {JSON.stringify(movieData2.actor_array)}</p> */}
+            </div>
+          )}
+          {movieData
+            ? movieData.actor_array.map((element) => {
+                console.log(element);
+                return (
+                  <Container centerContent key={element.person_id}>
+                    {/* <SimpleGrid columns={2}> */}
+                    {/* <Box
+                      border="0.5rem groove grey"
+                      bg="lightblue"
+                      fontSize="1.5rem"
+                      w="20rem"
+                      h="15rem"
+                    > */}
+                    <Link
+                      href={"/actors/" + element.person_id}
+                      color="black"
+                      textDecoration="none"
+                      _hover={{ color: "red", textDecoration: "underline" }}
+                    >
+                      {element.role === "ACTOR" ? (
+                        <div>
+                          <Text>Actor: {element.name}</Text>
+                          <Text>Character: {element.character}</Text>
+                        </div>
+                      ) : (
+                        <div>
+                          <Text>Director: {element.name}</Text>
+                        </div>
+                      )}
+                    </Link>
+                    {/* </Box> */}
+                    {/* <Box
+                    w="25rem"
+                    borderWidth="1rem"
+                    borderRadius="md"
+                    borderColor="gray"
+                    borderStyle="groove"
+                  >
+                    <Image
+                      w="100%"
+                      h="100%"
+                      src={element.Image}
+                      fallbackSrc="https://via.placeholder.com/325x500.png"
+                    />
+                  </Box>
+                </SimpleGrid> */}
+                  </Container>
+                );
+              })
+            : []}
           <Link fontSize="1.5rem" href="/actors/test">
             Click here to go to the Actor Page.
           </Link>
@@ -152,7 +219,7 @@ function Movie() {
           />
         </Box>
       </SimpleGrid>
-      <Divider border="null" w="80%" />
+      {/* <Divider border="null" w="80%" />
       <Text fontSize="1.5rem" color="red">
         Below this is the dynamic data test, it may look strange.
       </Text>
@@ -204,7 +271,7 @@ function Movie() {
               </Container>
             );
           })
-        : []}
+        : []} */}
     </div>
   );
 }
