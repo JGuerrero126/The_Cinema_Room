3570418684ee36543b23ec5258144cc72f09d569
Yang Cheng <annacheng09@hotmail.com>
2022-11-08 18:39:50 -0800

    added simple grid

--- client/src/components/pages/Movies.js
+++ client/src/components/pages/Movies.js
@@ -3,7 +3,14 @@ import axios from "axios";
 import { Input, Button } from "@chakra-ui/react";
 import { urlPrefix } from "../../utils/constants";
 import { useParams } from "react-router";
-import { Text, Link, Image, Container, Center} from "@chakra-ui/react";
+import {
+  Text,
+  Link,
+  Image,
+  Container,
+  Center,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import moment from "moment";
 
 function Movies() {
@@ -56,48 +63,57 @@ function Movies() {
       >
         Search
       </Button>
-      {searchedMovie
-        ? searchedMovie.results.map((element) => {
-            return (
-              <Container centerContent key={element.id} maxW="md">
-                <Link
-                  href={"/movies/" + element.id}
-                  color="white"
-                  textDecoration="none"
-                  fontFamily="Shindler"
-                  fontSize="1.5rem"
-                  transition="1s"
-                  _hover={{ color: "silver" }}
-                >
-                  <Text>
-                    <b>
-                      <u>{element.title}</u>
-                    </b>
-                  </Text>
-                  <Center mt="2rem">
-                    <Image
-                      border="1rem groove black"
-                      boxShadow="0rem 0rem 3rem lightyellow"
-                      _hover={{ boxShadow: "none", filter: "saturate(0%)" }}
-                      transition="3s"
-                      src={
-                        `https://image.tmdb.org/t/p/w500` + element.poster_path
-                      }
-                      fallbackSrc="https://via.placeholder.com/325x500.png"
-                    />
-                  </Center>
-                  <Text>
-                    Rating:
-                    <br />
-                    <b>{element.vote_average}</b>
-                  </Text>
-                  <Text textAlign="center">{element.overview}</Text>
-                  <Text>{moment(element.release_date).format("YYYY")}</Text>
-                </Link>
-              </Container>
-            );
-          })
-        : []}
+      <SimpleGrid
+        justify="center"
+        spacing="1.5rem"
+        minChildWidth="25rem"
+        ml="1rem"
+        mr="1rem"
+      >
+        {searchedMovie
+          ? searchedMovie.results.map((element) => {
+              return (
+                <Container centerContent key={element.id} maxW="md">
+                  <Link
+                    href={"/movies/" + element.id}
+                    color="white"
+                    textDecoration="none"
+                    fontFamily="Shindler"
+                    fontSize="1.5rem"
+                    transition="1s"
+                    _hover={{ color: "silver" }}
+                  >
+                    <Text>
+                      <b>
+                        <u>{element.title}</u>
+                      </b>
+                    </Text>
+                    <Center mt="2rem">
+                      <Image
+                        border="1rem groove black"
+                        boxShadow="0rem 0rem 3rem lightyellow"
+                        _hover={{ boxShadow: "none", filter: "saturate(0%)" }}
+                        transition="3s"
+                        src={
+                          `https://image.tmdb.org/t/p/w500` +
+                          element.poster_path
+                        }
+                        fallbackSrc="https://via.placeholder.com/325x500.png"
+                      />
+                    </Center>
+                    <Text>
+                      Rating:
+                      <br />
+                      <b>{element.vote_average}</b>
+                    </Text>
+                    <Text textAlign="center">{element.overview}</Text>
+                    <Text>{moment(element.release_date).format("YYYY")}</Text>
+                  </Link>
+                </Container>
+              );
+            })
+          : []}
+      </SimpleGrid>
     </div>
   );
 }
